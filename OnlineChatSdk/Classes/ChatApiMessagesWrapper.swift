4abb6708b26a1add99b752bc8a2c73804cd0b1a8
Andrey Blinov <bekannax@gmail.com>
2020-08-17 19:01:07 +0700

    update

---
+++ OnlineChatSdk/Classes/ChatApiMessagesWrapper.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class ChatApiMessagesWrapper {
+
+    init(_ response: Dictionary<String, Any>) {
+        print("ChatApiMessagesWrapper : \(response.debugDescription)")
+    }
+}
