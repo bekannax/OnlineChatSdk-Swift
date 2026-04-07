abb77e9324191e921d360b79f680e5cb7eaab058
bekannax <bekannax@gmail.com>
2026-04-07 15:25:27 +0700

    0.4.4

--- OnlineChatSdk.podspec
+++ OnlineChatSdk.podspec
@@ -1,13 +1,13 @@
 Pod::Spec.new do |s|
   s.name             = 'OnlineChatSdk'
-  s.version          = '0.4.3'
+  s.version          = '0.4.4'
   s.summary          = 'A small library containing a wrapper for the WKWebView.'
   s.swift_versions   = '5.0'
   s.homepage         = 'https://github.com/bekannax/OnlineChatSdk-Swift'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.authors          = { 'bekannax' => 'bekannax@gmail.com' }
   s.source           = { :git => 'https://github.com/bekannax/OnlineChatSdk-Swift.git', :tag => s.version.to_s }
-  s.ios.deployment_target = '13.0'
+  s.ios.deployment_target = '12.0'
   s.source_files = 'Sources/OnlineChatSdk/**/*'
   s.resource_bundles = {
     'OnlineChatSdk' => ['Sources/OnlineChatSdk/**/*']
--- Sources/OnlineChatSdk/CheckConnection.swift
+++ Sources/OnlineChatSdk/CheckConnection.swift
@@ -2,9 +2,6 @@ import Foundation
 
 @MainActor
 class CheckConnection {
-    
-    private var tasks: [Task<String, Error>] = []
-    private let lock = NSLock()
     private let check = "https://operator.me-talk.ru/cabinet/assets/operatorApplication/checkConnection.json"
     private let checkAlternative = "https://operator.verbox.me/cabinet/assets/operatorApplication/checkConnection.json"
     private var needUseAlternativeUrl: Bool? = nil
@@ -38,71 +35,158 @@ class CheckConnection {
         return false
     }
     
-    func cancel() {
-        lock.lock()
-        defer { lock.unlock() }
-        cancelAllTasks()
+    private func performSyncGetRequest(request: URLRequest) -> Bool {
+        let semaphore = DispatchSemaphore(value: 0)
+        var resultResponse: URLResponse?
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            resultResponse = response
+            semaphore.signal()
+        }
+        task.resume()
+        _ = semaphore.wait(timeout: .distantFuture)
+        if resultResponse != nil {
+            guard let httpResponse = resultResponse as? HTTPURLResponse else {
+                return false
+            }
+            return httpResponse.statusCode == 200
+        }
+        return false
     }
     
     private func requestsAsync(urls: [URL]) async throws -> String {
         for url in urls {
-            let task = Task<String, Error> {
-                var result: String = ""
-                try Task.checkCancellation()
-                var request = URLRequest(url: url, timeoutInterval: 2.0)
-                request.httpMethod = "GET"
-                request.addValue(UUID().uuidString, forHTTPHeaderField: "X-Request-ID")
-                do {
+            var request = URLRequest(url: url, timeoutInterval: 2.0)
+            request.httpMethod = "GET"
+            request.addValue(UUID().uuidString, forHTTPHeaderField: "X-Request-ID")
+            do {
+                if #available(iOS 15.0, *) {
                     let (_, response) = try await URLSession.shared.data(for: request)
                     guard let httpResponse = response as? HTTPURLResponse,
                           httpResponse.statusCode == 200 else {
-                        try Task.checkCancellation()
                         return ""
                     }
                     if httpResponse.statusCode == 200 {
-                        result = httpResponse.url?.absoluteString ?? ""
-                    } else {
-                        result = ""
+                        return url.absoluteString
                     }
-                } catch {
-                    result = ""
+                } else {
+                    if performSyncGetRequest(request: request) {
+                        return url.absoluteString
+                    }
                 }
-                try Task.checkCancellation()
-//                if result.contains(check) {
-//                    result = ""
-//                }
-                return result
-            }
-            tasks.append(task)
+            } catch {}
         }
-        
-        return try await withThrowingTaskGroup(of: String.self) { group in
-            for task in tasks {
-                group.addTask {
-                    return try await task.value
-                }
-            }
-            
-            guard let result = try await group.next() else {
-                return ""
-            }
-            if !result.isEmpty {
-                cancelAllTasks()
-                group.cancelAll()
-                return result
-            } else {
-                guard let result = try await group.next() else {
-                    return ""
-                }
-                cancelAllTasks()
-                group.cancelAll()
-                return result
-            }
-        }
-    }
-    
-    private func cancelAllTasks() {
-        tasks.forEach { $0.cancel() }
-        tasks.removeAll()
+        return ""
     }
 }
+
+
+
+//import Foundation
+//
+//@available(iOS 13.0, *)
+//@MainActor
+//class CheckConnectionOld {
+//    private var tasks: [Task<String, Error>] = []
+//    private let lock = NSLock()
+//    private let check = "https://operator.me-talk.ru/cabinet/assets/operatorApplication/checkConnection.json"
+//    private let checkAlternative = "https://operator.verbox.me/cabinet/assets/operatorApplication/checkConnection.json"
+//    private var needUseAlternativeUrl: Bool? = nil
+//    
+//    func getDomain() async -> String {
+//        let resultNeedUseAlternativeUrl = await isNeedUseAlternativeUrl()
+//        if resultNeedUseAlternativeUrl {
+//            return "admin.verbox.me"
+//        }
+//        return "admin.verbox.ru"
+//    }
+//    
+//    func isNeedUseAlternativeUrl() async -> Bool {
+//        if needUseAlternativeUrl != nil {
+//            return needUseAlternativeUrl!
+//        }
+//        guard let checkUrl = URL(string: check),
+//              let checkAlternativeUrl = URL(string: checkAlternative) else {
+//            return false
+//        }
+//        let sUrl = try? await requestsAsync(urls: [checkUrl, checkAlternativeUrl])
+//        if sUrl == nil {
+//            return false
+//        } else if sUrl!.contains(check) {
+//            needUseAlternativeUrl = false
+//            return false
+//        } else if sUrl!.contains(checkAlternative) {
+//            needUseAlternativeUrl = true
+//            return true
+//        }
+//        return false
+//    }
+//    
+//    func cancel() {
+//        lock.lock()
+//        defer { lock.unlock() }
+//        cancelAllTasks()
+//    }
+//    
+//    private func requestsAsync(urls: [URL]) async throws -> String {
+//        for url in urls {
+//            let task = Task<String, Error> {
+//                var result: String = ""
+//                try Task.checkCancellation()
+//                var request = URLRequest(url: url, timeoutInterval: 2.0)
+//                request.httpMethod = "GET"
+//                request.addValue(UUID().uuidString, forHTTPHeaderField: "X-Request-ID")
+//                do {
+//                    let (_, response) = try await URLSession.shared.data(for: request)
+//                    guard let httpResponse = response as? HTTPURLResponse,
+//                          httpResponse.statusCode == 200 else {
+//                        try Task.checkCancellation()
+//                        return ""
+//                    }
+//                    if httpResponse.statusCode == 200 {
+//                        result = httpResponse.url?.absoluteString ?? ""
+//                    } else {
+//                        result = ""
+//                    }
+//                } catch {
+//                    result = ""
+//                }
+//                try Task.checkCancellation()
+////                if result.contains(check) {
+////                    result = ""
+////                }
+//                return result
+//            }
+//            tasks.append(task)
+//        }
+//        
+//        return try await withThrowingTaskGroup(of: String.self) { group in
+//            for task in tasks {
+//                group.addTask {
+//                    return try await task.value
+//                }
+//            }
+//            
+//            guard let result = try await group.next() else {
+//                return ""
+//            }
+//            if !result.isEmpty {
+//                cancelAllTasks()
+//                group.cancelAll()
+//                return result
+//            } else {
+//                guard let result = try await group.next() else {
+//                    return ""
+//                }
+//                cancelAllTasks()
+//                group.cancelAll()
+//                return result
+//            }
+//        }
+//    }
+//    
+//    private func cancelAllTasks() {
+//        tasks.forEach { $0.cancel() }
+//        tasks.removeAll()
+//    }
+//}
